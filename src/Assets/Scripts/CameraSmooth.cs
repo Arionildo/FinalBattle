a7ee5643b8a8f442cc3da503cd5d2da3a6de98b9
ArionildoEscouto <arionildo6@gmail.com>
2018-05-07 23:41:00 -0300

    Initial Commit

---
+++ src/Assets/Scripts/CameraSmooth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmooth : MonoBehaviour {
+
+    public Transform target;
+    public float smooth = .1f;
+    public Vector3 offset;
+
+    private void Start() {
+        if (target == null)
+            target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void FixedUpdate() {
+        Vector3 cameraPosition = target.position + offset;
+        Vector3 smoothingPosition = Vector3.Lerp(transform.position, cameraPosition, smooth);
+
+        transform.position = smoothingPosition;
+        transform.LookAt(target);
+    }
+}
---
+++ src/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    public float movespeed, turnspeed, runspeed;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+
+        if (movespeed == 0)
+            movespeed = 10f;
+        if (turnspeed == 0)
+            turnspeed = 10f;
+        if (runspeed == 0)
+            runspeed = 20f;
+    }
+
+    private void FixedUpdate() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        Turn(horizontal, vertical);
+    }
+
+    private void Turn(float horizontal, float vertical) {
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        if (moveDirection != Vector3.zero) {
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            rb.MoveRotation(Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * turnspeed));
+        }
+    }
+
+    private void Move(float horizontal, float vertical) {
+        float currentSpeed = Input.GetButton("Run") ? runspeed : movespeed;
+
+        movement.Set(horizontal, 0f, vertical);
+        movement = movement.normalized * currentSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    
+}
